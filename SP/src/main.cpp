013e8426599fae0463d89a750c509bc93cc9ca2c
zarlior <zarlior@gmail.com>
2024-02-17 12:06:30 +0200

    Update main.cpp

--- SP/src/main.cpp
+++ SP/src/main.cpp
@@ -51,30 +51,12 @@ int main()
     }
     el.Stamp("Data loaded");
 
-    const int SIZE = 1000;
-    Buffer<std::complex<float>> src(SIZE), dst(SIZE);
-    for (int i = 0; i < SIZE; ++i)
-    {
-        float f = i * 0.1f * 2 * 3.14159265359f;
-        src[i] = std::complex<float>(std::sin(f), std::cos(f));
-    }
-    src.Upload();
-    el.Stamp("Data uploaded");
-
     // int t0 = program.Dispatch1D("convolve", num_of_samples, 256, *prev, *curr, *filter, *out, num_of_samples, filter_size, num_of_channels);
     int t0 = FFT::Dispatch(true, *curr, *out, num_of_samples);
-    // int t0 = FFT::Dispatch(false, *src, *dst, SIZE);
     std::cout << "Time: " << t0 << "us\n";
     el.Stamp("FFT");
 
     out.Download();
-    src.Download();
-    dst.Download();
-    auto &data = &dst;
-    std::sort(data.begin(), data.end(), [](const auto &a, const auto &b)
-              { return std::abs(a) > std::abs(b); });
-    std::reverse(data.begin(), data.end());
-
     el.Stamp("Data downloaded");
 
     int errors = Compare(&out, &result);
