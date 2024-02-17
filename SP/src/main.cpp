0fd70f598053c55569e89716d336950d51fa3232
zarlior <zarlior@gmail.com>
2024-02-17 12:04:29 +0200

    Update main.cpp

--- SP/src/main.cpp
+++ SP/src/main.cpp
@@ -1,7 +1,11 @@
 #include "cl/defines.h"
 #include "cl/Program.h"
 #include "cl/Buffer.hpp"
+#include "cl/FFT.h"
+#include "cl/Elapse.hpp"
+
 #include <complex>
+#include <cmath>
 
 const int num_of_channels = 20;
 const int samples_per_channel = 20000;
@@ -26,8 +30,11 @@ int Compare(const std::vector<std::complex<float>> &a, const std::vector<std::co
 
 int main()
 {
+
+    Elapse el("Main");
     std::cout << "----------------------------------------------------------\n";
     Context::getInstance();
+    FFT::getInstance();
     Program program("../../src/kernels/main.cl");
 
     Buffer<float> filter;
@@ -42,11 +49,37 @@ int main()
         std::cerr << "Invalid data size" << std::endl;
         return 1;
     }
+    el.Stamp("Data loaded");
 
-    int t0 = program.Dispatch1D("convolve", num_of_samples, 256, *prev, *curr, *filter, *out, num_of_samples, filter_size, num_of_channels);
+    const int SIZE = 1000;
+    Buffer<std::complex<float>> src(SIZE), dst(SIZE);
+    for (int i = 0; i < SIZE; ++i)
+    {
+        float f = i * 0.1f * 2 * 3.14159265359f;
+        src[i] = std::complex<float>(std::sin(f), std::cos(f));
+    }
+    src.Upload();
+    el.Stamp("Data uploaded");
+
+    // int t0 = program.Dispatch1D("convolve", num_of_samples, 256, *prev, *curr, *filter, *out, num_of_samples, filter_size, num_of_channels);
+    int t0 = FFT::Dispatch(true, *curr, *out, num_of_samples);
+    // int t0 = FFT::Dispatch(false, *src, *dst, SIZE);
     std::cout << "Time: " << t0 << "us\n";
+    el.Stamp("FFT");
+
     out.Download();
+    src.Download();
+    dst.Download();
+    auto &data = &dst;
+    std::sort(data.begin(), data.end(), [](const auto &a, const auto &b)
+              { return std::abs(a) > std::abs(b); });
+    std::reverse(data.begin(), data.end());
+
+    el.Stamp("Data downloaded");
+
     int errors = Compare(&out, &result);
+    el.Stamp("Compare");
+
     std::cout << "Errors: " << errors << std::endl;
 
     std::cout << "----------------------------------------------------------\n";
